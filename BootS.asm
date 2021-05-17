62c5e5896ddd87067e1b2f831748d291b8393050
Repo-LB <84280799+Repo-LB@users.noreply.github.com>
2021-05-17 11:58:40 -0400

    Real Mode Bootloader

--- BootS.asm
+++ BootS.asm
@@ -10,27 +10,27 @@ Main_Boot_Init:
     mov ss,ax                                     ;2; 64K Growing Downwards [0x007E00//0x017DFF]
     mov sp,0xFFFF                                 ;3; Lowest Address Right After This BootLoader	
     STI                                           ;1; Enable Interrupts 	
-	PUSHF                                         ;1; Save FLAGS To Stack	
-    JMP Disk_Reset                                ;2; JMP Moves Instruction Pointer [CS:IP] 	
+	PUSHF                                     ;1; Save FLAGS To Stack	
+    JMP Disk_Reset                                ;2; JMP Move Instruction Pointer [CS:IP] 	
     CLI                                           ;1; Disable Interrupts
-    HLT                                           ;1; Stops CPU Execution	
+    HLT                                           ;1; Stop CPU Execution	
 	
 Main_Boot_Success:
-	POPF	                                      ;1; Restore FLAGS From Stack
-	JMP 0x50:0x0                                  ;5; JMP Moves Instruction Pointer [CS:IP] 		
-	CLI                                           ;1; Disable Interrupts
-	HLT                                           ;1; Stops CPU Execution	
+	POPF	                                  ;1; Restore FLAGS From Stack
+	JMP 0x50:0x0                              ;5; JMP Move Instruction Pointer [CS:IP] 		
+	CLI                                       ;1; Disable Interrupts
+	HLT                                       ;1; Stop CPU Execution	
 	
 Disk_Reset:		
     mov ah,00h                                    ;2; INT 13h AH=00h (Reset Disk) DL=> Disk To Reset
     INT 13h                                       ;2; AH=> Return Code CF=> Set On Error
-	JNC Disk_ReadSector                           ;2; JMP IF NO ERROR (Carry Flag = 0)	
+	JNC Disk_ReadSector                       ;2; JMP IF NO ERROR (Carry Flag = 0)	
 	
 Disk_Reset_Fail:	
     CLC                                           ;1; Clear The Carry Flag
-	JMP Disk_Reset                                ;2; JMP Moves Instruction Pointer [CS:IP] 
+	JMP Disk_Reset                            ;2; JMP Move Instruction Pointer [CS:IP] 
     CLI                                           ;1; Disable Interrupts
-    HLT                                           ;1; Stops CPU Execution	
+    HLT                                           ;1; Stop CPU Execution	
 	
 Disk_ReadSector:
     mov ax, 0x50                                  ;3; Set Starting Address To Read Sectors Into [ES:BX] 
@@ -44,11 +44,11 @@ Disk_ReadSector:
 
     mov ah, 02                                    ;2; INT 13h AH=02h (Read Disk Sector) DL=> Disk To Read
     INT 13h                                       ;2; AH=> Return Code AL=> Sectors Read CF=> Set On Error
-	JNC Main_Boot_Success                         ;2; JMP IF NO ERROR (Carry Flag = 0)	
+	JNC Main_Boot_Success                     ;2; JMP IF NO ERROR (Carry Flag = 0)	
 	
 Disk_ReadSector_Fail:
     CLC                                           ;1; Clear The Carry Flag
-	JMP Disk_ReadSector                           ;2; JMP Moves Instruction Pointer [CS:IP] 
+	JMP Disk_ReadSector                       ;2; JMP Move Instruction Pointer [CS:IP] 
     CLI                                           ;1; Disable Interrupts
     HLT                                           ;1; Stops CPU Execution
 
